6df7c812e76f0508beacc153a13c383260a077b5
腰立辉 <yaolh@yonyou.com>
2020-03-17 15:44:34 +0800

    清理不用的文件，添加APP接口的共有方法

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,41 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	/**
+	 * App接口成功时的返回
+	 * @param array $data
+	 * @return string
+	 */
+	public function jsonSuccessData($data=[]){
+	
+	    return $this->jsonResponse(0, 'ok', $data);
+	}
+	
+	/**
+	 * APP接口异常时的返回
+	 * @param unknown $code
+	 * @param unknown $msg
+	 * @param array $data
+	 * @return string
+	 */
+	public function jsonData($code,$msg,$data=[]){
+	    return $this->jsonResponse($code, $msg, $data);
+	}
+	
+	/**
+	 * 返回一个json
+	 * @param unknown $code
+	 * @param unknown $msg
+	 * @param unknown $data
+	 * @return string
+	 */
+	
+	public function jsonResponse($code,$msg,$data){
+	    $content = [
+	        'code'  => $code,
+	        'msg'   => $msg,
+	        'data'  => $data
+	    ];
+	    return json_encode($content);
+	}
 }
